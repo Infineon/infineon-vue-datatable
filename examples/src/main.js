48d1ff8d847268c894157931a4b5fb64c56b433c
werther <kai.werther@infineon.com>
2022-09-19 15:26:04 +0200

    Removed bootstrap from library artifact and added it only to examples. Projects including this need to add bootstrap manually now.

--- examples/src/main.js
+++ examples/src/main.js
@@ -3,6 +3,8 @@ import { createApp } from 'vue';
 import router from './router/router';
 
 import './plugins/fontawesome';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
 
 import App from './App.vue';
 
